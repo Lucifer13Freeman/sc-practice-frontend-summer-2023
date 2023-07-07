29af83643a7abcd9f921f3c78b5b6fcca4fcd8c2
AntonioKabanov <anton_kabanov2002@mail.ru>
2023-07-08 02:53:11 +0300

    Created a second version of Navbar (Anton Kabanov) Deleted register-button from login pag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
 import { ModalModule } from './modules/core/modules/modal/modal.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, ModalModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, ModalModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
