0bcd7945aae328bd099aeccaec2e5efa07d24740
zh-iev <zh_iev@mail.ru>
2023-07-14 15:45:06 +0300

    added filter by tourneys

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -12,9 +12,10 @@ import { UsersComponent } from './components/users.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { MatInputModule } from '@angular/material/input';
 import { FilterUsersPipe } from './pipe/filter-users.pipe';
+import { FilterTourneysPipe } from './pipe/filter-tourneys.pipe';
 
 @NgModule({
-  declarations: [UsersComponent, FilterUsersPipe],
+  declarations: [UsersComponent, FilterUsersPipe, FilterTourneysPipe],
   imports: [
     UsersRoutingModule,
     MatIconModule,
